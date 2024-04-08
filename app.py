8f2bbf45ea0f7f9df43eb15a5836b6f91d1006c4
VrnikaJain <166123197+VrnikaJain@users.noreply.github.com>
2024-04-08 14:49:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -318,12 +318,12 @@ elif selected_analysis == "Appointment day difference VS No Show":
     ax.set_xlabel('Appointment Day - Scheduled Day Difference (Days)')
     ax.set_ylabel('No Show')
     ax.set_yticks([0, 1])  # Set y-ticks to correspond to the 'No-show' values
-    ax.set_yticklabels([no_show_mapping[val] for val in ax.get_yticks()])
+    ax.set_yticklabels([no_show_mapping.get(val, val) for val in ax.get_yticks()])
     ax.set_title('Appointment day difference VS No Show')
     
     # Add labels to specific points on the plot
     for i, row in filtered_df.iterrows():
-        ax.text(row['Appointment_Scheduled_Difference'], 0 if row['No-show'] == 'No' else 1, row['No-show'], color='black', ha='center', va='bottom')
+        ax.text(row['Appointment_Scheduled_Difference'], 0 if row['No-show'] == 'No' else 1, row['No-show'], color='grey', ha='center', va='bottom')
     
     # Calculate and add text for total no-shows
     total_no_shows = filtered_df['No-show'].value_counts().get('Yes', 0)
